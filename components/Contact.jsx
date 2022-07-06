505b18a098d673732f6f1a328c616898cc5d2d59
kimamovic21 <imamovic.kerim@gmail.com>
2022-07-06 15:36:57 +0200

    izmjene
    Contact.jsx komponenta

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -13,42 +13,47 @@ const Contact = () => {
                            className='mt-3'/>
         </div>
 
-        <form id='my-form'
-              action='https://getform.io/f/dca02e7e-afff-4d0f-98a4-7ccc66ea5d33'
+        <form action='https://getform.io/f/dca02e7e-afff-4d0f-98a4-7ccc66ea5d33'
               method='POST'
               className='max-w-[600px] m-auto'>
+
+            {/* Name and surname */}
             <div className='grid grid-cols-2 gap-2'>
                 <input className='border shadow-lg p-3 outline-none focus:scale-105 ease-out duration-200'
                        type="text" 
                        placeholder='Name' 
-                       name='my-form'
+                       name='Name'
                 />
                 <input className='border shadow-lg p-3 outline-none focus:scale-105 ease-out duration-200'
                        type="text" 
                        placeholder='Surname' 
-                       name='my-form'
+                       name='Surname'
                 />
             </div>
+
+            {/* Email, subject, message */}
             <input className='border shadow-lg p-3 w-full my-2 outline-none focus:scale-105 ease-out duration-200'
                    type="email" 
                    placeholder='Email'
-                   name='my-form' 
+                   name='Email' 
             />
             <input className='border shadow-lg p-3 w-full my-2 outline-none focus:scale-105 ease-out duration-200'
                    type="text" 
                    placeholder='Subject' 
-                   name='my-form'
+                   name='Subject'
             />
             <textarea className='border shadow-lg p-3 w-full outline-none focus:scale-105 ease-out duration-200'
                       cols="30" 
                       rows="10"
                       placeholder='Message'
-                      name='my-form'
+                      name='Message'
             />
+
             <button className='border shadow-lg p-3 w-full mt-2 bg-[#0073f7] text-[#efefef] rounded-xl 
                              hover:bg-[#0a10bc] ease-in-out duration-300 hover:scale-105'>
                 Submit
             </button>
+
         </form>
 
     </div>
@@ -67,4 +72,5 @@ export default Contact;
 // 3. dodajemo klase ostalim elementima
 // 4. dodajemo atribute form elementu
 // 5. uredujemo submit button element na kraju contact stranice
+// 6. input elementima sam dodao name atribut zbog forme
 
